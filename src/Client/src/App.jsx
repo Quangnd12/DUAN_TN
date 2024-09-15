a7bf1e46c8264f6e14cab3ef15ce6935fd10c899
QUANG <quangnguyenkz123@gmail.com>
2024-09-15 15:25:40 +0700

    Làm sidebar mở/đóng theo kích thước
    Sử dụng thuộc tính Drawer/core, Box trong thư viện M.UI, dùng thuộc tính useState, useEffect cho tính năng

--- src/Client/src/App.jsx
+++ src/Client/src/App.jsx
@@ -1,43 +1,63 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
-import SideBar from './components/sidebar/SideBar.component';
-import HomePage from './pages/homepage/HomePage';
-import Header from './components/header/Header';
-import SearchPage from './pages/searchpage/SearchPage';
-import Artist from './pages/artist/Artist';
-import Login from './pages/auth/login';
-import Register from './pages/auth/register';
-import ForgotPass from './pages/auth/forgotPass';
+import { useState, useEffect } from "react";
+import SideBar from "./components/sidebar/SideBar.component";
+import HomePage from "./pages/homepage/HomePage";
+import Header from "./components/header/Header";
+import SearchPage from "./pages/searchpage/SearchPage";
+import Artist from "./pages/artist/Artist";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import ForgotPass from "./pages/auth/forgotPass";
 import Track from "./pages/track/Track";
-import Content from './pages/content/Content';
-import InfoClient from './pages/info-client/Info-client';
-
+import Content from "./pages/content/Content";
+import InfoClient from "./pages/info-client/Info-client";
 
 function Client() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(!isMobile);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newIsMobile = window.innerWidth < 768;
+      setIsMobile(newIsMobile);
+      setIsSidebarOpen(!newIsMobile);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="App max-h-dvh bg-black ">
-      <div className='flex gap-2'>
-        <div style={{ width: "300px" }}>
-          <SideBar />
-        </div>
-        <div className='flex-1'>
-          <Header />
-          <div className='overflow-y-auto scrollbar-custom ' style={{  height: "620px" }}>
+      <div className="flex gap-2">
+        <div>{!isMobile && <SideBar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />}</div>
+        <div className={`flex-1 flex flex-col ${isSidebarOpen ? 'ml-0' : 'ml-16'} transition-all duration-300`}>
+        <Header toggleSidebar={toggleSidebar} />
+          <div
+            className="overflow-y-auto scrollbar-custom "
+            style={{ height: "620px" }}
+          >
             <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/search' element={<SearchPage />} />
-              <Route path='/artist' element={<Artist />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/forgot' element={<ForgotPass />} />
-              <Route path='/content' element={<Content />} />
-              <Route path='/info' element={<InfoClient />} />
-              <Route path='/track' element={<Track />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/artist" element={<Artist />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/forgot" element={<ForgotPass />} />
+              <Route path="/content" element={<Content />} />
+              <Route path="/info" element={<InfoClient />} />
+              <Route path="/track" element={<Track />} />
             </Routes>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 export default Client;
--- src/Client/src/components/sidebar/SideBar.component.jsx
+++ src/Client/src/components/sidebar/SideBar.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
@@ -7,70 +7,132 @@ import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import QueueMusicIcon from "@mui/icons-material/QueueMusic";
 import LanguageIcon from "@mui/icons-material/Language";
-import Logo from "../../assets/images/logo.jpg";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import MenuIcon from "@mui/icons-material/Menu";
+import Tooltip from "@mui/material/Tooltip";
+import Logo from "../../assets/images/logo.png";
 
 const SideBar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isOpen, setIsOpen] = useState(() => {
+    const saved = localStorage.getItem("sidebarOpen");
+    return saved !== null ? JSON.parse(saved) : true;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("sidebarOpen", JSON.stringify(isOpen));
+  }, [isOpen]);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex flex-col h-full gap-4">
-      <div className="flex flex-col gap-4 bg-zinc-900 p-2 rounded-md">
+    <div
+      className={`flex flex-col h-screen transition-all mr-1 duration-300 ease-in-out ${
+        isOpen ? "w-72" : "w-16"
+      } bg-zinc-900 p-2 rounded-md`}
+    >
+      <div className="flex items-center justify-start mb-4">
         <Link to={"/"}>
-          <img width={50} height={50} src={Logo} alt="logo" />
-        </Link>
-        <Link to={"/search"}>
-          <div className="flex items-center gap-2 hover:bg-gray-600 rounded py-2">
-            <SearchIcon fontSize="large" sx={{ color: "white" }} />
-            <div className="text-white font-bol">Search</div>
-          </div>
-        </Link>
-        <Link to={"/"}>
-          <div className="flex items-center gap-2 hover:bg-gray-600 rounded py-2">
-            <HomeIcon fontSize="large" sx={{ color: "white" }} />
-            <div className="text-white font-bold">Home</div>
-          </div>
-        </Link>
-        <Link to={"/favorite"}>
-          <div className="flex items-center gap-2 hover:bg-gray-600 rounded py-2">
-            <FavoriteIcon fontSize="large" sx={{ color: "white" }} />
-            <div className="text-white font-bold">Favorite</div>
-          </div>
-        </Link>
-        <Link to={"/library"}>
-          <div className="flex items-center gap-2 hover:bg-gray-600 rounded py-2">
-            <LibraryMusicIcon fontSize="large" sx={{ color: "white" }} />
-            <div className="text-white font-bold">Your library</div>
-          </div>
+          <img
+            width={isOpen ? 70 : 60}
+            height={isOpen ? 70 : 60}
+            src={Logo}
+            alt="logo"
+            className="transition-all duration-300"
+          />
         </Link>
       </div>
-      <div className="flex flex-col gap-4 bg-zinc-900 p-2 rounded-md">
-        <Link to={"/track"}>
-          <div className="flex items-center gap-2 p-2 hover:bg-gray-600 rounded py-2">
-            <QueueMusicIcon fontSize="large" sx={{ color: "white" }} />
-            <div className="text-white font-bold">Playlist</div>
-          </div>
-        </Link>
-        <Link to={"/top-rank"}>
-          <div className="flex items-center gap-2 p-2 hover:bg-gray-600 rounded py-2">
-            <StarRateIcon fontSize="large" sx={{ color: "white" }} />
-            <div className="text-white font-bold">Top rank</div>
-          </div>
-        </Link>
-      </div>
-      <div className="flex flex-col gap-2 bg-zinc-900 p-4 py-8 rounded-md">
-        <div className="text-white font-bold p-3">
-          Pay the fat together <br />
-          and get lost in the <br />
-          music
-        </div>
-        <div
-          className="flex items-center gap-3 rounded-full w-max p-2 text-white font-bold bg-black hover:scale-105 cursor-pointer transition-all"
-          style={{ border: "1px solid white" }}
+      <div className="relative left-0 my-2 ">
+        <button
+          onClick={toggleSidebar}
+          className="text-white hover:bg-gray-600 hover:rounded-md px-3 py-3"
         >
-          <LanguageIcon />
-          <span className="ml-0">English</span>
+          {isOpen ? (
+            <Tooltip title="Collapse the library">
+              {" "}
+              <MenuIcon fontSize="medium" />
+            </Tooltip>
+          ) : (
+            <Tooltip title="Expand the library">
+              <ArrowForwardIosIcon />
+            </Tooltip>
+          )}
+        </button>
+      </div>
+      <NavItem
+        to="/search"
+        icon={<SearchIcon />}
+        text="Search"
+        isOpen={isOpen}
+        tooltipText="Search"
+      />
+      <NavItem to="/" icon={<HomeIcon />} text="Home" isOpen={isOpen} />
+      <NavItem
+        to="/favorite"
+        icon={<FavoriteIcon />}
+        text="Favorite"
+        isOpen={isOpen}
+      />
+      <NavItem
+        to="/library"
+        icon={<LibraryMusicIcon />}
+        text="My library"
+        isOpen={isOpen}
+      />
+      <NavItem
+        to="/track"
+        icon={<QueueMusicIcon />}
+        text="Playlist"
+        isOpen={isOpen}
+      />
+      <NavItem
+        to="/top-rank"
+        icon={<StarRateIcon />}
+        text="Top rank"
+        isOpen={isOpen}
+      />
+
+      <div className="mt-auto">
+        {isOpen && (
+          <div className="text-white font-bold p-3 mb-4">
+            Burn together, get lost in the music heals
+          </div>
+        )}
+        <div
+          className={`flex items-center gap-3 rounded-full w-max p-2 mb-5 font-bold transition-all cursor-pointer ${
+            isClicked ? "bg-white text-black" : "bg-black text-white"
+          } hover:scale-105 ${isOpen ? "" : "justify-center"}`}
+          onClick={() => setIsClicked(!isClicked)}
+        >
+          {" "}
+          <Tooltip title="Change language">
+            <LanguageIcon className={isClicked ? "text-black" : "text-white"} />
+            {isOpen && (
+              <span className="ml-0">
+                {isClicked ? "Tiếng Việt" : "English"}
+              </span>
+            )}
+          </Tooltip>
         </div>
       </div>
     </div>
   );
 };
+
+const NavItem = ({ to, icon, text, isOpen }) => (
+  <Link to={to}>
+    <Tooltip title={text} placement="bottom">
+      <div className="flex items-center gap-2 hover:bg-gray-600 rounded py-2 px-2 mb-2">
+        {React.cloneElement(icon, {
+          fontSize: "large",
+          sx: { color: "white" },
+        })}
+        {isOpen && <div className="text-white font-bold">{text}</div>}
+      </div>
+    </Tooltip>
+  </Link>
+);
 
 export default SideBar;
